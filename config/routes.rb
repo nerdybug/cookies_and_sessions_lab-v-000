7aebc88255209ad55512e76a545253b1f947f7ec
Meredith Reece <mardebugger@gmail.com>
2018-08-08 20:25:57 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :products, only: [:index, :add]
+  post '/' => 'products#add'
+  root 'products#index'
 end
